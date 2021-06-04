59aeb2c91a02ff85b7d92e161d747473ae4541c0
Thomas Buol <tom7now@hotmail.com>
2021-06-04 15:12:32 -0500

    DestinationTrip API Controller

---
+++ TripPlanner/TripPlanner.Web/Models/DestinationTripModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.Web.Models
+{
+    public class DestinationTripModel
+    {
+        public int DestinationID { get; set; }
+        public int TripID { get; set; }
+    }
+}
