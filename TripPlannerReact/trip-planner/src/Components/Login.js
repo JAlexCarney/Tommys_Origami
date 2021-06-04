2ff59a96e1eef8998ebe81f6f49676af4b05c0f7
Thomas Buol <tom7now@hotmail.com>
2021-06-04 13:39:04 -0500

    React Login Page complete

--- TripPlannerReact/trip-planner/src/Components/Login.js
+++ TripPlannerReact/trip-planner/src/Components/Login.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-let Login = () => {
+let Login = (props) => {
+    const [loginModel, setLoginModel] = useState({});
+
+    let handleChange = (event) => {
+        let newLoginModel = {...loginModel};
+        newLoginModel[event.target.name] = event.target.value;
+        setLoginModel(newLoginModel);
+    }
+    let onSubmit = (event) => {
+        event.preventDefault();
+        const init = {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body: JSON.stringify(loginModel)
+        };
+        fetch("https://localhost:44365/api/auth/login", init)
+            .then(response => {
+                if(response.status !== 200){
+                    return Promise.reject("Was not able to login.")
+                }
+                return response.json();
+            })
+            .then(json => props.getToken(json.token))
+            .catch(console.log);
+    
+    }
     return (
         <div>
-            Hello World!
-            <form className="form-group" id="login">
+            <form onSubmit={onSubmit} className="form-group" id="login">
                 <div>
-                    <label htmlFor="email" >Email</label>
-                    <input type="text" />
+                    <label htmlFor="identifier" >Email/Username</label>
+                    <input type="text" name="identifier" onChange={handleChange}/>
                 </div>
+                <div>
+                    <label htmlFor="password" >Password</label>
+                    <input type="password" name="password" onChange={handleChange}/>
+                </div>
+                <button type="submit" >Login</button>
+                <button type="button" onClick={() => {props.changePage("CreateUser")}}>Create User</button>
             </form>
         </div>
     )
--- TripPlannerReact/trip-planner/src/Components/TripPlanner.js
+++ TripPlannerReact/trip-planner/src/Components/TripPlanner.js
@@ -3,11 +3,19 @@ import Login from './Login';
 import CreateUser from './CreateUser';
 
 let TripPlanner = () => {
-    const [state, setState] = useState("CreateUser");
+    const [state, setState] = useState("LoginPage");
+    const [token, setToken] = useState("");
 
+    function ChangePage(page){
+        setState(page);
+    }
+    function GetToken(newToken){
+        console.log(newToken);
+        setToken(newToken);
+    }
     switch(state){
-        case "loginPage":
-            return <Login />;
+        case "LoginPage":
+            return <Login changePage={ChangePage} getToken={GetToken}/>;
         case "CreateUser":
             return <CreateUser />;
     }
