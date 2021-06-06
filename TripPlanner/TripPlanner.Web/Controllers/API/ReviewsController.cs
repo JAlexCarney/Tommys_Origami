d212cf06266da952a925bfc365b83e081876fb43
upadhyer <radhika.upadhye@gmail.com>
2021-06-06 16:21:54 -0500

    basic review webapi setup

---
+++ TripPlanner/TripPlanner.Web/Controllers/API/ReviewsController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TripPlanner.Core;
+using TripPlanner.Core.Entities;
+using TripPlanner.Core.Interfaces;
+
+namespace TripPlanner.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : Controller
+    {
+
+        private readonly IReviewRepository _reviewRepository;
+
+        public ReviewsController(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+
+        //add, edit, remove, get, getbyuser, getbydestination
+
+        [HttpGet(Name = "GetReviewsByDestination")]
+        [Route("/api/reviews/{destinationID}")]
+        public IActionResult GetReviewsByDestination(int destinationID)
+        {
+            var result = _reviewRepository.GetByDestination(destinationID);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+
+        [HttpGet(Name = "GetReviewsByUser")]
+        [Route("/api/reviews/{userID}")]
+        public IActionResult GetReviewsByUser(Guid userID)
+        {
+            var result = _reviewRepository.GetByUser(userID);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet(Name = "GetReview")]
+        public IActionResult GetReview(Review model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _reviewRepository.Get(model.DestinationID, model.UserID);
+
+                if (result.Success)
+                {
+                    return Ok(result.Data);
+                }
+                return BadRequest(result.Message);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        [Route("/api/reviews")]
+        public IActionResult AddReview(Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                Response<Review> result = _reviewRepository.Add(review);
+
+                if (result.Success)
+                {
+                    //check
+                    return CreatedAtRoute(nameof(GetReviewsByDestination), new { id = result.Data.DestinationID }, review);
+                }
+                return BadRequest(result.Message);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public IActionResult EditReviews(Review review )
+        {
+            if (!_reviewRepository.Get(review.DestinationID, review.UserID).Success)
+            {
+                return NotFound($"Review by user {review.UserID} could not be found for destination {review.DestinationID}");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var result = _reviewRepository.Edit(review);
+
+                if (result.Success)
+                {
+                    return Ok();
+                }
+                return BadRequest(result.Message);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete]
+        //check reviewmodel
+        public IActionResult RemoveReviews(Review model)
+        {
+            if (!_reviewRepository.Get(model.DestinationID, model.UserID).Success)
+            {
+                return NotFound($"Review by user {model.UserID} could not be found for destination {model.DestinationID}");
+            }
+
+            var result = _reviewRepository.Remove(model.DestinationID, model.UserID);
+
+            if (result.Success)
+            {
+                return Ok();
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
