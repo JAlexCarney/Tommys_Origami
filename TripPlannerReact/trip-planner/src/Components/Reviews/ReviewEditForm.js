2a202fa223ece151f645e8b37d459fe2aa4f98b6
upadhyer <radhika.upadhye@gmail.com>
2021-06-09 20:49:16 -0500

    edit lookskinda right

--- TripPlannerReact/trip-planner/src/Components/Reviews/ReviewEditForm.js
+++ TripPlannerReact/trip-planner/src/Components/Reviews/ReviewEditForm.js
@@ -1,12 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
+//can take out
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
 let Component = (props) => 
 {
     const [state, setState] = useState([]);
-    const [review, setReview] = useState({});
-    const [reviews, setReviews] = useState([]);
+    const [editedReviews, setEditedReviews] = useState([]);
+
+    useEffect(() => {
+        setState(props.review);
+    }, [props.review]);
 
     const handleChange = (event) => {
         let newState = { ...state };
@@ -29,57 +33,40 @@ let Component = (props) =>
         setState(newState);
     }
 
-    let handleSelection = (event) => {
-        
-        let newReview = { ...review };
-        newReview["destinationID"] = event.target.value;
-        newReview["destination"] = event.target.options[event.target.selectedIndex].text;
-        setReview(newReview);
-    }
-
-    let destinationOptions = (list) =>
-    {
-        return list.map((destination) => {
-            return(
-                <option name="id" value={destination.destinationID} >{destination.city + " - " + destination.country}</option>
-                
-            );
-        });
+    const editReviews = (reviews) => {
+        setEditedReviews(reviews);
     }
 
     return (
         <div className="form">
-            <h3 className="form-header">Adding Review</h3>
-        <form onSubmit={(event) => {event.preventDefault(); console.log(state); props.handleAdd(state);}}>
+            <h3 className="form-header">Editing Review for Destination{" " + props.review.destinationID}</h3>
+
             <div className="form-field">
                                     <label htmlfor="destination">Destination</label>
-                                    <select onChange={handleSelection}>
-                                        <option selected disabled>Choose Destination</option>
-                                        {destinationOptions(state)}
-                                    </select>
+                                    <label htmlfor="destination">{props.review.destinationID}</label>
                                 </div>
             <div className="form-field">
                 <label htmlFor="rating">Rating</label>
                 <div class="wrapper">
-                    <input name="ratingRadio" type="radio" id="st1" value="1" />
+                    <input name="rating" type="radio" id="st1" value="1" />
                     <label for="st1"></label>
-                    <input name="ratingRadio" type="radio" id="st2" value="2" />
+                    <input name="rating" type="radio" id="st2" value="2" />
                     <label for="st2"></label>
-                    <input name="ratingRadio" type="radio" id="st3" value="3" />
+                    <input name="rating" type="radio" id="st3" value="3" />
                     <label for="st3"></label>
-                    <input name="ratingRadio" type="radio" id="st4" value="4" />
+                    <input name="rating" type="radio" id="st4" value="4" />
                     <label for="st4"></label>
-                    <input name="ratingRadio" type="radio" id="st5" value="5" />
+                    <input name="rating" type="radio" id="st5" value="5" />
                     <label for="st5"></label>
                 </div>
             </div>
             <div className="form-field">
                 <label htmlFor="description">Description</label>
-                <input type="text" className="form-control inputs" defaultValue={"Great trip!"}/>
+                <input type="text" className="form-control inputs" onChange={handleChange} defaultValue={props.review.description}/>
             </div>
-            <button className="btn btn-primary btn-submit" type="submit">Confirm Add</button><br/>
+            <button className="btn btn-primary btn-submit" type="submit">Confirm Edit</button><br/>
             <button className="btn btn-secondary btn-submit" onClick={props.exitView}>Cancel</button>
-        </form>
+
         </div>
     );
 }
